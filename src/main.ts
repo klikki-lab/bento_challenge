82644f34d6cda53d1200627a3d873778685d3f40
klikki-lab <klikki.lab@gmail.com>
2024-04-05 07:10:42 +0900

    ゲーム時間と音量の修正

--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,14 @@ import { TitleScene } from "./title_scene/titleScene";
 export function main(param: GameMainParameterObject): void {
     g.game.vars.version = "0.0.1";//バージョン更新忘れずに!!
 
-    g.game.audio.music.volume = 0.15;
-    g.game.audio.sound.volume = 0.3;
+    g.game.audio.music.volume = 0.3;
+    g.game.audio.sound.volume = 0.2;
     g.game.vars.gameState = {
         score: 0,
         playThreshold: 1,
         clearThreshold: undefined,
     };
 
-    //const totalTimeLimit = param.sessionParameter.totalTimeLimit ?? 75;
-    
     const titleScene = new TitleScene(7);
     titleScene.onFinish.add(() => {
         g.game.pushScene(new GameScene(param, 60));
