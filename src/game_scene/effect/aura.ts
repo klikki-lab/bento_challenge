fff55e57c9dc9e1f49708d43b06fe74528843597
klikki-lab <klikki.lab@gmail.com>
2024-04-25 11:42:07 +0900

    オーラエフェクトの追加

---
+++ src/game_scene/effect/aura.ts
@@ -0,0 +1,17 @@
+export class Aura extends g.FrameSprite {
+
+    constructor(scene: g.Scene) {
+        super({
+            scene: scene,
+            src: scene.asset.getImageById("img_aura"),
+            srcWidth: 128,
+            srcHeight: 192,
+            width: 128,
+            height: 192,
+            opacity: 0.25,
+            anchorX: .5,
+            anchorY: .5,
+            frames: [0, 1, 2],
+        });
+    }
+}
