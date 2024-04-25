a52194c5bffb8c5406d73aee01dd1e16cca84e60
klikki-lab <klikki.lab@gmail.com>
2024-04-25 12:22:37 +0900

    音量の修正

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import { GameScene } from "./game_scene/gameScene";
 import { TitleScene } from "./title_scene/titleScene";
 
 export function main(param: GameMainParameterObject): void {
-    g.game.audio.music.volume = 0.3;
-    g.game.audio.sound.volume = 0.3;
+    g.game.audio.music.volume = 0.25;
+    g.game.audio.sound.volume = 0.25;
     g.game.vars.gameState = {
         score: 0,
         playThreshold: 1,
